0858f2ee579312fe20ec664fb8777c117dec7141
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-10-24 18:42:27 -0400

    first commit, mongoDB folder!

---
+++ startingMongo/index.js
@@ -0,0 +1,11 @@
+/*
+Starting to use MONGO... 
+
+JavaScript (NODE.JS) will conduct runtime environment opertations.
+
+An express server will be running up this app. Some JavaScript code will be 
+written just for some code repition of ES6 concepts i learned today.
+(OCT-24th-2022)
+*/
+
+
